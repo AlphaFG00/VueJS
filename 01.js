65698b847d8240356b1761349b3e690e54ae14cd
AlphaFG00 <96fernando.guerrero@gmail.com>
2019-05-21 15:30:01 -0500

    avance hasta componentes, clase 7

--- 01.js
+++ 01.js
@@ -12,8 +12,64 @@ const app = new Vue({
 		{nombre: 'Pera', estado: 'verde'},
 		{nombre: 'manzana', estado: 'rojo'},
 		{nombre: 'mango', estado: 'amarillo'}
-		]
+		],
+
+		//CLASE 2: CONEXION DE HTML CON JAVASCRIPT
+		nuevaFruta: '',
+
+		//clase 3: key up
+		nuevoColor:'',
+		//clase 4: v-bind class
+		fondo: 'bg-warning',
+		//clase 5: Computed
+		mensaje: 'Vue.js',
+		contador: 0
+		
 
 
-	}
+
+	},
+	computed:{
+		invertido(){
+
+			return this.mensaje.split('').reverse().join('');
+		},
+		//clase 5:computed
+		color(){
+			return{
+				'bg-success' :this.contador <=10,
+				'bg-warning' :this.contador >10 && this.contador < 20,
+				'bg-danger' :this.contador >=20
+			} 
+
+		}
+
+
+
+
+
+
+	},
+ 	methods: {
+ 		agregarFruta(){
+ 			//accede a propiedades de tda la instancia
+
+ 			this.frutas2.push({
+ 				nombre: this.nuevaFruta, cantidad: 0
+
+
+ 			});
+
+ 			//clase 3: key up de vue js
+ 			this.nuevaFruta= '';
+ 		},
+ 		setColor(){
+ 			this.frutas2.push({
+ 				estado: this.nuevoColor,cantidad: 0
+ 			});
+ 			this.nuevoColor='';
+
+
+ 		}
+ 	}
 })
